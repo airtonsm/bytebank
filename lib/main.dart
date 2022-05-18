22b6d2bab8a54cd73d03f9a7d5654812d8c8ea69
jose.morais <jose.morais@netprecision.com>
2022-05-17 21:13:08 -0300

    aula-06: criando classes e metodos

--- lib/main.dart
+++ lib/main.dart
@@ -3,24 +3,7 @@ import 'package:flutter/material.dart';
 void main() => runApp(
   MaterialApp(
     home: Scaffold(
-      body: Column(
-        children: const [
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.monetization_on),
-              title: Text('Rs 100,00'),
-              subtitle: Text('1.000,00'),
-            )
-          ),
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.monetization_on),
-              title: Text('Rs 200,00'),
-              subtitle: Text('1.000,00'),
-            )
-          ),
-        ],
-      ),
+      body: ListaTransferencias(),
       appBar: AppBar(title: Text('Transferências'),),
       floatingActionButton: FloatingActionButton(
         onPressed: () {  },
@@ -30,4 +13,42 @@ void main() => runApp(
   )
 );
 
-// parei na aula 08
+class ListaTransferencias extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children:  [
+        ItemTransferencia(Transferencia(100.00, 1004)),
+        ItemTransferencia(Transferencia(4500.00, 4010)),
+        ItemTransferencia(Transferencia(1500.00, 1018)),
+      ],
+    );
+  }
+}
+
+class ItemTransferencia extends StatelessWidget{
+
+  final Transferencia _transferencia;
+
+  ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+   return Card(
+       child: ListTile(
+         leading: Icon(Icons.monetization_on),
+         title: Text(_transferencia.valor.toString()),
+         subtitle: Text(_transferencia.numeroConta.toString()),
+       )
+   );
+  }
+
+}
+
+
+class Transferencia{
+  final double valor;
+  final int numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
+}
